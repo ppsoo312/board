9aa1738446dc1da5ec3c63c90dd70fbc8588e4d0
YounSooPark <YounSooPark@pys>
2023-03-09 10:05:09 +0900

    one

---
+++ SMTR/src/main/java/com/sy/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.sy.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sy.smtr.HomeController;
+
+@Controller
+public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value="/")
+	public String home() {
+		return "home";
+	}
+
+}
---
+++ SMTR/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ include file="layout/property.jsp" %>
+<!DOCTYPE html>
+<html lang="en-us"><head>
+  <meta charset="utf-8">
+  <title>Reader | Hugo Personal Blog Template</title>
+</head>
+<body>
+	<%@ include file="layout/header.jsp" %>
+	
+	<%@ include file="main/popularChef.jsp" %>
+</body>
+</html>
